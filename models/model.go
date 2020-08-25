f521359a84c21a3742639b9ce2e219640b8908b0
SteveChan <steve.d.chan@qq.com>
2020-08-25 22:29:32 +0800

    feat: update model

--- models/model.go
+++ models/model.go
@@ -37,20 +37,35 @@ func init() {
 
 // 管理员权限等级及级别名称
 type Permission struct {
-	Id		int		                               // 权限登记id
-	Level	string	`json:"level" orm:"size(30)"`  // 权限级别
-	Name	string	`json:"name" orm:"size(20)"`   // 权限名称
-	Admin	[]*Admin `orm:rel(m2m)`                // orm映射 一个权限可以被多个管理员所拥有
+	Id		int		                               		// 权限登记id
+	Level	string		`json:"level" orm:"size(30)"`  	// 权限级别
+	Name	string		`json:"name" orm:"size(20)"`   	// 权限名称
+	Admin	[]*Admin 	`orm:"rel(m2m)"`                // orm映射 一个权限可以被多个管理员所拥有
 }
 
 // 地区城市表
 type City struct {
-
+	Id 			int 		`json:"id"`  					//城市id
+	CityName 	string 		`json:"name" orm:"size(20)"`   	//城市名称
+	PinYin 		string 		`json:"pin_yin"`  				//城市名称拼音
+	Longitude 	float32 	`json:"longitude"` 	 			// 城市经度
+	Latitude 	float32 	`json:"latitude"`  				// 城市纬度
+	AreaCode 	string 		`json:"area_code"`   			// 城市地区编码
+	Abbr		string 		`json:"abbr"`    				// 城市拼音缩写
+	User 		[]*User 	`orm:"reverse(many)"`   		// orm映射 一个城市可以有多个用户
+	Admin 		[]*Admin 	`orm:"reverse(many)"`   		// orm映射 一个城市可以有多个管理员
 }
 
 // 管理员表
 type Admin struct {
-
+	Id 			int 			`json:"id"`   						// 管理员编号id
+	UserName 	string 			`json:"user_name" orm:"size(12)"`   // 管理员用户名
+	CreateTime 	string 			`json:"create_time"`   				// 记录添加时间
+	Status 		int 			`json:"status"`  					// 管理员状态
+	Avatar 		string			`json:"avatar" orm:"size(50)"`  	// 管理员头像
+	Pwd 		string 			`json:"pwd"`   						// 管理员密码
+	Permission 	[]*Permission 	`orm:"reverse(many)"`   			// 一个管理员可以有多种权限
+	City 		*City 			`orm:"rel(fk)"`  					// orm映射 管理员所在城市
 }
 
 // 用户信息表
