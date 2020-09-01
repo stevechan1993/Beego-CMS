28896ef91a4d774325da751d6fbe4577205716a6
SteveChan <steve.d.chan@qq.com>
2020-09-01 10:30:39 +0800

    feat: add respCode && update main

--- main.go
+++ main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"github.com/astaxie/beego"
-	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/stevechan/Beego-CMS/routers"
 )
 
 func main() {
+
+	beego.AddAPPStartHook(func() error {
+		beego.Info("自定义配置")
+		return nil
+	})
+
+	// 静态资源路径映射
+	beego.SetStaticPath("manage/static", "static")
+
+	// 图片资源路径映射
+	beego.SetStaticPath("img", "img")
+
+	// 监听
 	beego.Run()
 }
-
