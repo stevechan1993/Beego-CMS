e673f6ce2555c487ab4257233ae066011d8b3d37
SteveChan <steve.d.chan@qq.com>
2020-10-07 11:15:38 +0800

    feat:添加商户模块

--- models/adminModel.go
+++ models/adminModel.go
@@ -3,14 +3,14 @@ package models
 /**
 从Admin数据库实体转换为前端请求resp的json格式
  */
-func (admin *Admin) AdminToRespDesc() interface{} {
+func (this *Admin) AdminToRespDesc() interface{} {
 	respDesc := map[string]interface{} {
-		"user_name": 	admin.UserName,
-		"id": 			admin.Id,
-		"create_time": 	admin.CreateTime,
-		"status": 		admin.Status,
-		"avatar": 		admin.Avatar,
-		"city": 		admin.City.CityName,
+		"user_name": 	this.UserName,
+		"id": 			this.Id,
+		"create_time": 	this.CreateTime,
+		"status": 		this.Status,
+		"avatar": 		this.Avatar,
+		"city": 		this.City.CityName,
 		"admin": 		"管理员",
 	}
 	return respDesc
