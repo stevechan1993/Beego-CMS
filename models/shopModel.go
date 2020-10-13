ef4144d0302e1f1c568d453f7908e44afca9957e
SteveChan <steve.d.chan@qq.com>
2020-10-13 18:38:47 +0800

    feat:添加shop模型

--- models/shopModel.go
+++ models/shopModel.go
@@ -1,3 +1,18 @@
 package models
 
-
+/**
+ * 数据映射为json
+ */
+func (this *Shop) ShopToRespDesc() interface{} {
+	respDesc := map[string]interface{} {
+		"id":	this.Id,
+		"name": this.Name,
+		"address": this.Address,
+		"phone": this.Phone,
+		"status": this.Status,
+		"recent_order_num": this.RecentOrderNum,
+		"rating_count": this.RatingCount,
+		"rating": this.Rating,
+	}
+	return respDesc
+}
